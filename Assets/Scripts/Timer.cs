137d1c96c3bab65a936c500e01bba27a7a6756ab
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2024-10-09 13:55:51 +0100

    Add boilerplate Timer script and meta files

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private Stopwatch _stopwatch = new Stopwatch();
+
+    public bool IsActive()
+    {
+        return _stopwatch.IsRunning;
+    }
+
+    public bool TransitionStopwatch()
+    {
+        switch (_stopwatch.IsRunning)
+        {
+            // 2 states: start, stop
+            case true:
+                _stopwatch.Stop();
+                return true;
+            case false:
+                _stopwatch.Start();
+                return true;
+        }
+    }
+
+    public bool ResetStopwatch()
+    {
+// Will stop the stopwatch if active
+// Will reset back to 0
+        _stopwatch.Reset();
+        return true;
+    }
+
+    public string GetStopwatchTime()
+    {
+        // Will return a string of the current stopwatch time
+        return _stopwatch.ToString();
+    }
+
+}
