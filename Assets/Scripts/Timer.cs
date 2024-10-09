4b3ee717a41932f4e7e75ef4b79b3b56d789c28c
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2024-10-09 14:55:43 +0100

    Added formatting and initial working timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
     private Stopwatch _stopwatch = new Stopwatch();
-
+    private TextMeshProUGUI _stopwatchText;
+    
     public bool IsActive()
     {
         return _stopwatch.IsRunning;
@@ -28,8 +32,8 @@ public class Timer : MonoBehaviour
 
     public bool ResetStopwatch()
     {
-// Will stop the stopwatch if active
-// Will reset back to 0
+        // Will stop the stopwatch if active
+        // Will reset back to 0
         _stopwatch.Reset();
         return true;
     }
@@ -38,7 +42,17 @@ public class Timer : MonoBehaviour
     {
         // Will return a string of the current stopwatch time
         // Should produce "Current Time: 0.000"
-        return "Current Time: " + _stopwatch.ToString();
+        return String.Format("Current Time: {0:00}s {1:000}ms", _stopwatch.Elapsed.TotalSeconds, _stopwatch.Elapsed.Milliseconds);
     }
 
+    void Start()
+    {
+        _stopwatchText = GetComponent<TextMeshProUGUI>();
+        TransitionStopwatch();
+    }
+
+    private void Update()
+    {
+        _stopwatchText.text = GetStopwatchTime();
+    }
 }
