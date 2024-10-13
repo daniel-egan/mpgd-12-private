fb1c6ebf72f86a21cf93b1a1e49e1788d48e505f
AdamBeji <adambeji11@gmail.com>
2024-10-13 10:21:27 +0100

    Enemy moves
    Enemy moves towards the player position but still isn't moving as a fish (i.e drops to the bottom of the tank and rolls)

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Transform targetObj;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(this.transform.position, targetObj.position, 10 * Time.deltaTime);
     }
 }
