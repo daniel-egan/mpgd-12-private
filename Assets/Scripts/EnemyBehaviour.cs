7ee121a6a8619ff15dcebbe68c7b67ec0b9ea061
AdamBeji <adambeji11@gmail.com>
2024-10-20 11:57:02 +0100

    Added a hit count
    whenever a player is hit by an enemy the hitcount increases

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -8,6 +8,7 @@ public class NewBehaviourScript : MonoBehaviour
     private Rigidbody rb;
     public float pushBackForce = 4f;
     private CharacterController characterController;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -16,10 +17,11 @@ public class NewBehaviourScript : MonoBehaviour
 
     void Update()
     {
+
+        // Raycast to check if enemy has hit an object and if so stops
+        // Used https://www.youtube.com/watch?v=B34iq4O5ZYI and ChatGPT
         Vector3 direction = (targetObj.position - transform.position).normalized;
-
         float distanceToMove = 3 * Time.deltaTime;
-
         RaycastHit hit;
         if (Physics.Raycast(transform.position, direction, out hit, distanceToMove))
         {
@@ -27,15 +29,24 @@ public class NewBehaviourScript : MonoBehaviour
             {
                 return;
             }
-
         }
+
+
+        // Enemy moves towards a player
+        // Used https://docs.unity3d.com/ScriptReference/Vector3.MoveTowards.html
+        // and https://docs.unity3d.com/ScriptReference/Transform-position.html
         transform.position = Vector3.MoveTowards(this.transform.position, targetObj.position, distanceToMove);
     }
 
+    // Collision detection used https://docs.unity3d.com/ScriptReference/Collider.OnCollisionEnter.html 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            // Vector created with direction and velocity is calculated to move the player
+            // Used https://docs.unity3d.com/ScriptReference/Vector3.html
+            // Used https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+
             Vector3 direction = (transform.position - collision.transform.position).normalized;
             Vector3 velocity = direction * pushBackForce;
             characterController.Move(velocity * Time.deltaTime);
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,24 +1,44 @@
+using System.Collections;
+using System.Collections.Generic ;
 using UnityEngine;
+using UnityEngine.UI ;
+using TMPro;
 
 public class PlayerCollision : MonoBehaviour
 {
     public float pushBackForce = 200f;
     private CharacterController characterController;
 
+    public TextMeshProUGUI Hitcount;
+    private int count;
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        UpdateHitText();
 
     }
 
+    // Collision detection used https://docs.unity3d.com/ScriptReference/Collider.OnCollisionEnter.html 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            // Vector created with direction and velocity is calculated to move the player
+            // Used https://docs.unity3d.com/ScriptReference/Vector3.html
+            // Used https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+
             Vector3 direction = (transform.position - collision.transform.position).normalized;
             Vector3 velocity = direction * pushBackForce;
+            count++;
             characterController.Move(velocity * (Time.deltaTime*20));
+            UpdateHitText();
         }
+    }
+
+    private void UpdateHitText()
+    {
+        Hitcount.text = "Hitcount : " + count.ToString();
     }
 }
 
