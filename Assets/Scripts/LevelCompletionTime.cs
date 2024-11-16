b0fe66c49a465ef10ee6eb2b5075306f71ca4309
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2024-11-16 11:10:19 +0000

    Started foundations for the level completion times

--- Assets/Scripts/LevelCompletionTime.cs
+++ Assets/Scripts/LevelCompletionTime.cs
@@ -11,11 +11,10 @@ public class LevelCompletionTime : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        
         // Get the most recent time
-        var levelCompletionTime = PlayerPrefs.GetString("LevelCompletionTime");
+        var levelCompletionTime = PlayerPrefs.GetFloat("LevelCompletionTime");
         print(levelCompletionTime);
+        
         // Get the most recent level completed
         var levelCompletionName = PlayerPrefs.GetString("LevelCompletionName");
         print(levelCompletionName);
@@ -23,9 +22,34 @@ public class LevelCompletionTime : MonoBehaviour
         TextMeshProUGUI timeText = GameObject.Find("Time").GetComponent<TextMeshProUGUI>();
         timeText.text = $"{levelCompletionName} completed in {levelCompletionTime}";
 
-        // Check against top 3 times for this level
+        // Get the 3 best times for the level
+        float[] times = GetTop3Times(levelCompletionName);
         
-    float[] GetTop3Times(string levelCompletionName)
+        // Compare the top 3 to see if completion time was quicker
+        CompareandUpdateTop3Times(times, levelCompletionTime);
+    }
+
+    private void CompareandUpdateTop3Times(float[] times, float levelCompletionTime)
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            if (times[i] == 0)
+            {
+                // Then we know that the player has not completed the level at this index
+                // And then we can just insert at this current index
+            }
+            
+            // Check if the levelCompletionTime is quicker than the time at the current index
+            if (times[i] > levelCompletionTime)
+            {
+                
+            }
+            
+            // Otherwise it has been completed and is slower than the current time so we move on
+        }
+    }
+
+    private float[] GetTop3Times(string levelCompletionName)
     {
         string topTimesString = PlayerPrefs.GetString($"{levelCompletionName}_TIMES", "0,0,0");
         float[] times = topTimesString.Split(',').Select(float.Parse).ToArray();
