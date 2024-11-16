aef36920b4b573148fb2582133bf66bd3b87f66a
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2024-11-16 10:40:02 +0000

    Added method to retrieve top 3 completion times from PlayerPrefs

--- Assets/Scripts/LevelCompletionTime.cs
+++ Assets/Scripts/LevelCompletionTime.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -24,5 +25,10 @@ public class LevelCompletionTime : MonoBehaviour
 
         // Check against top 3 times for this level
         
+    float[] GetTop3Times(string levelCompletionName)
+    {
+        string topTimesString = PlayerPrefs.GetString($"{levelCompletionName}_TIMES", "0,0,0");
+        float[] times = topTimesString.Split(',').Select(float.Parse).ToArray();
+        return times;
     }
 }
