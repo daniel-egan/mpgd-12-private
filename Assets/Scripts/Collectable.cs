13088d58fc32d3fab4021aa01f52e975d06c26eb
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2024-12-01 16:26:24 +0000

    Refactor collectable unlocking logic to use CollectableManager

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -6,20 +6,20 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     public string id;
-    public string collectableName;
-    public string collectableDescription;
 
-    private void UnlockCollectable()
-    {
-        PlayerPrefs.SetInt(id, 1); // Means the achievement is now unlocked
-        PlayerPrefs.Save();
-        gameObject.SetActive(false);
-    }
+    // private void UnlockCollectable()
+    // {
+    //     // PlayerPrefs.SetInt(id, 1); // Means the achievement is now unlocked
+    //     // PlayerPrefs.Save();
+    //     // gameObject.SetActive(false);
+    //     
+    // }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.name != "Player") return;
 
-        UnlockCollectable();
+        CollectableManager.Instance.UnlockCollectable(id);
+        gameObject.SetActive(false);
     }
 }
