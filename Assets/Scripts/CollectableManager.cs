40a5a4af1df523d22352381201f4b7a38a9b0dd2
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2024-12-01 15:36:56 +0000

    Got initial JSON loading working Will correctly get the 5 achievements saved in the JSON file and store them within CollectableItems. Now need to get the unlock working so that the isUnlocked goes to true, and test that the save occurs correctly so that achievements can only be unlocked once

--- Assets/Scripts/CollectableManager.cs
+++ Assets/Scripts/CollectableManager.cs
@@ -1,15 +1,96 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public class CollectableItem
+{
+    public string id;
+    public string name;
+    public string description;
+    public bool isUnlocked;
+}
+
+[Serializable]
+public class CollectableItemList
+{
+    public List<CollectableItem> collectables;
+}
 
 public class CollectableManager : MonoBehaviour
 {
-    public static CollectableManager Instance;
+    public static CollectableManager Instance { get; private set; }
+    public List<CollectableItem> CollectableItems { get; private set; }
 
     private void Awake()
     {
-        if (Instance == null)
-            Instance = this;
-        else
+        if (Instance != null && Instance != this)
+        {
             Destroy(gameObject);
+            return;
+        }
+
+            Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        CollectableItems = GetCollectableItems();
+        print(CollectableItems.Count);
+    }
+
+    private List<CollectableItem> GetCollectableItems()
+    {
+        string savedPlayerPrefs = PlayerPrefs.GetString("collectables", string.Empty);
+
+        if (string.IsNullOrEmpty(savedPlayerPrefs))
+        {
+            print("LOADING FROM JSON"); 
+            List<CollectableItem> jsonStored = LoadJsonFile();
+            return jsonStored;
+        }
+
+        else
+        {
+            print("LOADING FROM PLAYERPREFS");
+            List<CollectableItem> jsonStored = JsonUtility.FromJson<List<CollectableItem>>(savedPlayerPrefs);
+            return jsonStored;
+        }
+
+    }
+
+    private List<CollectableItem> LoadJsonFile()
+    {
+        TextAsset jsonFile = Resources.Load<TextAsset>("achievements");
+
+        try
+        {
+            CollectableItemList jsonStored = JsonUtility.FromJson<CollectableItemList>(jsonFile.text);
+            return jsonStored.collectables;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Error parsing achievements.json: {e.Message}");
+            return new List<CollectableItem>();
+        }
+
+    }
+
+    private CollectableItem findItemById(string id)
+    {
+        CollectableItem element = CollectableItems.Find(element => element.id == id);
+        return element;
+    }
+
+    public void UnlockCollectable(string id)
+    {
+        var collectable = findItemById(id);
+        collectable.isUnlocked = true;
+        SavePlayerPrefs();
+    }
+
+    private void SavePlayerPrefs()
+    {
+        var json = JsonUtility.ToJson(CollectableItems, true);
+        PlayerPrefs.SetString("collectables", json);
+        PlayerPrefs.Save();
     }
 }
