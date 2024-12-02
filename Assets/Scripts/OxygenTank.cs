4ceb2c7cccbd7716ab250e10db912a7365fd3210
Haidar Mohsen <haidarf2008@yahoo.com>
2024-12-02 11:11:51 +0000

    Add NPC

---
+++ Assets/Scripts/OxygenTank.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OxygenTankPickup : MonoBehaviour
+{
+    public TextMeshProUGUI Completion;
+    public GameObject Diver;
+    public TextMeshProUGUI Objectives;
+    public TextMeshProUGUI Speech;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            // Update the objective text
+            Completion.text = "Thank you so much!!! You will be rewarded";
+            Speech.text = "";
+            BoxCollider boxCollider = Diver.GetComponent<BoxCollider>();
+            Destroy(boxCollider);
+
+            // Destroy the oxygen tank after pickup
+            Destroy(gameObject);
+
+        }
+    }
+}
