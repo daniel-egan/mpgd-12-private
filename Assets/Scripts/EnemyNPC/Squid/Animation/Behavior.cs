aff2a04b35a8738555adb7a0251e9ab05538dfcd
AdamBeji <adambeji11@gmail.com>
2024-12-04 14:10:25 +0000

    squid looks at player only whilst attacking

---
+++ Assets/Scripts/EnemyNPC/Squid/Animation/Behavior.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBehavior : MonoBehaviour
+{
+    public Animator npcAnimator;
+    public GameObject player;
+    public float rotationSpeed = 200f;
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (gameObject.CompareTag("EnemySquid"))
+            {
+                Vector3 directionToPlayer = player.transform.position - transform.position;
+                Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+            }
+
+            // Trigger the Attack animation
+            npcAnimator.SetTrigger("Attack");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            npcAnimator.SetTrigger("Swim");
+        }
+    }
+}
+
