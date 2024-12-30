e9b1f59e12cb071fb01c4503ac82b976cfa754a4
AdamBeji <adambeji11@gmail.com>
2024-12-30 07:21:58 +0000

    friendly npc movement code cleanup

--- Assets/Scripts/NPC's/FriendlyNPC/Crabs/FriendlyNPCMovement.cs
+++ Assets/Scripts/NPC's/FriendlyNPC/Crabs/FriendlyNPCMovement.cs
@@ -15,6 +15,9 @@ public class FriendlyNPCMovement : MonoBehaviour
         FindObjectives();
     }
 
+    // When you enter the crabs trigger zone the animation will start playing
+    // This animation guides the player to the first part of the tutorial
+    // This teaches them how to initially play the game
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -26,6 +29,7 @@ public class FriendlyNPCMovement : MonoBehaviour
         }
     }
 
+    // Animation stops when the player is too far away from the crab
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -33,11 +37,14 @@ public class FriendlyNPCMovement : MonoBehaviour
             npcAnimator.speed = 0f;
         }
     }
+
+    // Sets the objectives when ypu start the level
     private void FindObjectives()
     {
         Objectives.text = "Locate your crab friend near their house";
     }
 
+    // Objectives change when you are following the crab
     private void FollowObjectives()
     {
         Objectives.text = "Follow your friend to locate the key";
