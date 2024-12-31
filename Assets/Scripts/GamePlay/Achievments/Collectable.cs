5c8438a2342714330069cccf39299f35021554e1
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2024-12-31 14:21:08 +0000

    Remove commented-out UnlockCollectable method from Collectable class

--- Assets/Scripts/GamePlay/Achievments/Collectable.cs
+++ Assets/Scripts/GamePlay/Achievments/Collectable.cs
@@ -7,14 +7,6 @@ public class Collectable : MonoBehaviour
 {
     public string id;
 
-    // private void UnlockCollectable()
-    // {
-    //     // PlayerPrefs.SetInt(id, 1); // Means the achievement is now unlocked
-    //     // PlayerPrefs.Save();
-    //     // gameObject.SetActive(false);
-    //     
-    // }
-
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.name != "Player") return;
